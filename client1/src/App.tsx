e55147b2f0b52c803eb97cff9549d427747532a4
RM798 <rameshmaurya798@gmail.com>
2024-01-03 23:48:59 +0530

    Changes done.

--- client1/src/App.tsx
+++ client1/src/App.tsx
@@ -1,8 +1,21 @@
+const products =[
+  {name: 'product1', price: 100.00},
+  {name: 'product2', price: 200.00},
+  {name: 'product2', price: 300.00},
+]
+
+
 
 function App() {
   return (
     <div>
       <h1>Re-Store</h1>
+
+      <ul>
+        {products.map((item, _index) =>(
+          <li key={_index}>{item.name} - {item.price}</li>
+        ) )}
+      </ul>
     </div>
   )
 }
